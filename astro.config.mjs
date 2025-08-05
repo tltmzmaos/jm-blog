36f5722b80ab684a6bfb3b0f329ac64d5e94c0e6
tltmzmaos <tltmzmaos@gmail.com>
2025-08-04 21:49:49 -0700

    Fix navigation links for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://tltmzmaos.github.io',
-    base: '/jm-blog',
+    site: 'https://jongmin.me',
+    base: '/',
     integrations: [
         tailwind(),
         mdx(),
